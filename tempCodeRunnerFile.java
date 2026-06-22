233287b92662ecd7eaec6fe7651f4f090f58f1f0
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-06-22 10:07:03 +0530

    Update tempCodeRunnerFile.java
    linear search

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -402,6 +402,27 @@ class Main {
     }
 }
 
+Linear search::
+
+    import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int key=102;
+        int val=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==key){
+                val=arr[i];
+            }
+        }
+        System.out.print(val);
+    }
+}
 
 
 
