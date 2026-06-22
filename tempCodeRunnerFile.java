c93ba35c794b37b0bbbe20e4efab756202aa156a
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-06-22 09:53:58 +0530

    Update tempCodeRunnerFile.java
    printing the values

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -385,7 +385,22 @@ class Solution {
         return maxVal;
     }
 }
+printing the elements::
 
+    import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
 
 
 
