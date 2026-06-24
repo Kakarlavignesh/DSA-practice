f92245126d80ad9bf9f21140016c9cd832171243
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-06-24 09:06:06 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -543,47 +543,31 @@ class Main {
     }
 }
 
+Longest Subarray with Sum K::
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int n=arr.length;
+        int maxLen=0;
+        int sum=0;
+        int j=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+ 
+            if(sum==k){
+                maxLen=i+1;
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
+            }
+            if(map.containsKey(sum-k)){
+                maxLen=Math.max(maxLen,i-map.get(sum-k));
+            }
+        }
+        return maxLen;
+    }
+}
 
 
 
