c2a061423ef31b2ba8d6b69ccded1037917efa48
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-06-30 10:19:07 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -995,6 +995,28 @@ class NumArray {
     }
 }
 
+// pivot Index..
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        int totalSum=0;
+        for(int i=0;i<n;i++){
+            totalSum+=nums[i];
+        }
+        int leftSum=0;
+        int rightSum=0;
+        for(int i=0;i<n;i++){
+            rightSum=totalSum-leftSum-nums[i];
+            if(leftSum==rightSum){
+                return i;
+            }
+            leftSum+=nums[i];
+        }
+        return -1;
+    }
+}
+
 
 
 
