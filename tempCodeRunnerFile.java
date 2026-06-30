21fcb4b58b52a8051447c3ff70c40cc6f5fa9daf
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-06-30 10:39:28 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1029,6 +1029,24 @@ class Solution {
     }
 }
 
+// Highest altitude...
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int n=gain.length;
+        int[] pre=new int[n+1];
+        pre[0]=0;
+        int max=pre[0];
+        for(int i=1;i<=n;i++){
+            pre[i]=pre[i-1]+gain[i-1];
+            if(pre[i]>max){
+                max=pre[i];
+            }
+        }
+        return max;
+    }
+}
+
 
 
 
