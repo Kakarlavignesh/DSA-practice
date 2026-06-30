fa1efd6193366f5713515cd165866af167c417f9
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-06-30 10:31:11 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1017,6 +1017,18 @@ class Solution {
     }
 }
 
+// Running sum of 1d Array
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            nums[i]=nums[i-1]+nums[i];
+        }
+        return nums;
+    }
+}
+
 
 
 
