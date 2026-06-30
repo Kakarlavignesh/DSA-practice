311ef6655116545fd55f35c4829ff6527859d855
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-06-30 11:34:19 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1047,7 +1047,26 @@ class Solution {
     }
 }
 
+// SubArrays with sum k::
 
+class Solution {
+    public int cntSubarrays(int[] arr, int k) {
+        // code here
+        int n=arr.length;
+        int count=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
+}
 
 
 
