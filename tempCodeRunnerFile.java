95a3aef0f431aac95fc3bcc6547e3400252ec37b
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-07-02 09:36:01 +0530

    Add isValid method for validating parentheses
    Implement a method to check for valid parentheses in a string.

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1275,6 +1275,29 @@ class StackQueue {
     }
 }
 
+// Valid paranthesis::
+
+class Solution{
+    public boolean isValid(String s){
+        int n=s.length();
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            if(st.isEmpty()){
+                return false;
+            }
+            else if(!st.isEmpty()){
+                if(ch==')' && st.pop()!='(' || ch==']' && st.pop()!='[' || ch=='}' && st.pop()!='{'){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
 
 
 
