f14ac11468543a07429857973c3ebefdfdb04b0c
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-07-12 15:01:31 +0530

    Implement character frequency counter in Java
    Added a Java program to count character frequencies in a string and print the most frequent characters.

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1401,6 +1401,29 @@ class Main {
     }
 }
 
-
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        String s="programming";
+        int n=s.length();
+        // char ch=s.toCharArray();
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int max=0;
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            if(entry.getValue()>max){
+                max=entry.getValue();
+            }
+        }
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            if(entry.getValue()==max){
+                System.out.println(entry.getKey()+" "+entry.getValue());
+            }
+        }
+    }
+}
 
 
