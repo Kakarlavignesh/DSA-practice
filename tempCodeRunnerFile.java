62a5f3dd8232ee0440fad05a181cb81305b14364
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-07-15 16:26:19 +0530

    Add containsNearbyDuplicate method to Solution class

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1556,8 +1556,24 @@ Sliding window Problem 2..//mastering sliding window..
         return maxVowel;
     }
 }
+SLiding window ...209 lc//
 
-
+    class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n=nums.length;
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+            if(set.size()>k){
+                set.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+}
 
 
 
