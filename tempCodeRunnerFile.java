2acda3a006ad8eb0ac0b34c3fe8880803f72bb12
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-07-25 10:16:54 +0530

    Add countBinarySubstrings method to Solution class
    Implement a method to count binary substrings in a given string.

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1767,6 +1767,25 @@ class Main {
         }
     }
 }
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int n=s.length();
+        int curr=1;
+        int prev=0;
+        int res=0;
+        for(int i=1;i<n;i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                curr++;
+            }
+            else{
+                res+=Math.min(prev,curr);
+                prev=curr;
+                curr=1;
+            }
+        }
+        return res+Math.min(prev,curr);
+    }
+}
 
 
 
