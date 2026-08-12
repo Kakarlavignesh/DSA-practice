c8fb8010709519b2e2b39d09a15d44dc88a8cf8a
KAKARLA VIGNESH REDDY <148783663+Kakarlavignesh@users.noreply.github.com>
2026-08-12 20:37:06 +0530

    Implement merge sort algorithm in Java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1947,7 +1947,61 @@ class Solution {
     }
 }
 
-
+import java.util.*;
+class Main {
+    public static void mergeSort(int[] arr,int left,int right){
+        if(left<right){
+            int mid=left+(right-left)/2;
+            mergeSort(arr,left,mid);
+            mergeSort(arr,mid+1,right);
+            merge(arr,left,mid,right);
+        }
+    }
+    public static void merge(int[] arr,int left,int mid,int right){
+        int n1=mid-left+1;
+        int n2=right-mid;
+        int[] lArr=new int[n1];
+        int[] rArr=new int[n2];
+        for(int i=0;i<n1;i++){
+            lArr[i]=arr[left+i];
+        }
+        for(int i=0;i<n2;i++){
+            rArr[i]=arr[mid+i+1];
+        }
+        int i=0;
+        int j=0;
+        int k=left;
+        while(i<n1 && j<n2){
+            if(lArr[i]<=rArr[j]){
+                arr[k]=lArr[i];
+                i++;
+            }
+            else{
+                arr[k]=rArr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n1){
+            arr[k]=lArr[i];
+            i++;
+            k++;
+        }
+        while(j<n2){
+            arr[k]=rArr[j];
+            j++;
+            k++;
+        }
+    }
+    public static void main(String[] args){
+        int[] arr={5,2,1,4,7,9};
+        int n=arr.length;
+        mergeSort(arr,0,n-1);
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+}
 
 
 
